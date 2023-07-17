91c7ea16af3b031f7459dcb4a37cca4bda9b281b
Mazhanov <mazhanov1997@yandex.ru>
2023-07-17 15:37:17 +0300

    edits1

--- stats/stats-server/src/main/java/ru/practicum/core/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/core/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.core;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,13 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException exception) {
         log.error("Exception ItemUnavailableException {}", exception.getMessage(), exception);
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(final MissingServletRequestParameterException exception) {
+        log.error("Exception MissingServletRequestParameterException {}", exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
 
